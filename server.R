08d55bc2d0823ddc13420eeae8f88a5fa07606be
Kara Lee <lee125@uw.edu>
2018-03-01 21:41:11 -0800

    Add reactive

--- server.R
+++ server.R
@@ -2,6 +2,15 @@ library(shiny)
 
 my.server <- function(input, output) {
   
+violence <- reactive ({
+  return (input$violence)
+})
+
+max.precip <- reactive ({
+  return (input$max.precip)
+})
+
+
 ############### BOSTON #################
   
 ############## SAN FRAN ###############
