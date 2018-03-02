9f6ff1f5775bc00f59aa9654b9c816616d8006d5
Stephen Moxley <smox98@uw.edu>
2018-03-01 21:43:25 -0800

    Change ui to show checkboxes and add app file

---
+++ app.R
@@ -0,0 +1,5 @@
+source("server.R")
+source("ui.R")
+
+# Creates the Shiny app
+shinyApp(ui = ui, server = server)
--- ui.R
+++ ui.R
@@ -6,16 +6,16 @@ ui <- fluidPage(
     sidebarPanel(
       sliderInput('max.precip', label = "Max amount of precipitation", 
                   min = 0, max = 5, value = 5),
-      selectInput('violence', label = "Crimes Included", choices = c("Violent" = "Violent", 
+      checkboxGroupInput('violence', label = "Crimes Included", choices = c("Violent" = "Violent", 
                                                                      "Nonviolent" = "Nonviolent"), 
-                  multiple = TRUE, selected = c("Violent", "Nonviolent"))
+                  selected = c("Violent", "Nonviolent"))
     ),
     mainPanel(
       tabsetPanel( type = "tabs",
                    tabPanel("Home"),
                    tabPanel("Boston", plotOutput("Boston.bar"), plotOutput("Boston.map")), 
                    tabPanel("San Francisco", plotOutput("SF.bar"), plotOutput("SF.map")), 
-                   tabPanel("Los Angeles", plotOut("LA.bar"), plotOutput("LA.bar")),
+                   tabPanel("Los Angeles", plotOutput("LA.bar"), plotOutput("LA.bar")),
                    tabPanel("Chicago", plotOutput("Chicago.bar"), plotOutput("Chicago.map"))
       )
     )
