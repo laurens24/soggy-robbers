7be98e2ea626c8f9fab52bd2b7c14011b84ca9a8
Kara Lee <lee125@uw.edu>
2018-03-06 17:29:42 -0800

    Add the ui.R and server.R file with the bar plot code

--- ui.R
+++ ui.R
@@ -4,18 +4,19 @@ ui <- fluidPage(
   titlePanel('Creative Title'),
   sidebarLayout(
     sidebarPanel(
-      sliderInput('max.precip', label = "Max amount of precipitation", 
-                  min = 0, max = 5, value = 5),
-      checkboxGroupInput('violence', label = "Crimes Included", choices = c("Violent" = "Violent", 
-                                                                     "Nonviolent" = "Nonviolent"), 
-                  selected = c("Violent", "Nonviolent"))
+      sliderInput('precip', label = "Max amount of precipitation", 
+                  min = 0, max = 5, value = c(0, 5)), 
+      radioButtons('violence', label = "Crimes Included", choices = c("Violent" = "Violent", 
+                                                                     "Nonviolent" = "Nonviolent", 
+                                                                     "Both" = "Both")
+                  )
     ),
     mainPanel(
       tabsetPanel( type = "tabs",
                    tabPanel("Home"),
                    tabPanel("Boston", plotOutput("Boston.bar"), plotOutput("Boston.map")), 
                    tabPanel("San Francisco", plotOutput("SF.bar"), plotOutput("SF.map")), 
-                   tabPanel("Los Angeles", plotOutput("LA.bar"), plotOutput("LA.bar")),
+                   tabPanel("Los Angeles", plotOutput("LA.bar", click = 'bar.click'), plotOutput("LA.map")),
                    tabPanel("Chicago", plotOutput("Chicago.bar"), plotOutput("Chicago.map"))
       )
     )
