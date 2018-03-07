f0d2d82ccce86ac025cb816d3965bf3517f176ff
Kara Lee <lee125@uw.edu>
2018-03-06 20:47:47 -0800

    Add LA bar and map descriptions

--- ui.R
+++ ui.R
@@ -29,8 +29,14 @@ my.ui <- fluidPage(
                    ),
                    tabPanel("Los Angeles",
                             tabsetPanel(
-                              tabPanel("Precipitation Chart", plotOutput("LA.bar")),
-                              tabPanel("Crime Map", plotOutput("LA.map"))
+                              tabPanel("Precipitation Chart", plotOutput("LA.bar"),
+                                       p("The bar chart above shows the average number of crimes corresponding to each range of precipitation
+                                         levels. The user can choose the crime type and the range of precipitation from the sidebar. It should be noted that
+                                         Los Angeles had 0 precipitation for 90% of the data gathered from January 1st, 2016 to February 8th, 2018.
+                                         Since only 10% of the data corresponded to precipitation levels above 0, the results may not be the most accurate
+                                         and cannot be used to imply causation")),
+                              tabPanel("Crime Map", plotOutput("LA.map"),
+                                       p("The map of Los Angeles above shows locations of 200 randomly selected crimes."))
                             )
                    ),
                    tabPanel("Chicago",
