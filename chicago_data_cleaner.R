272de1ee4cd1c9f89f1f78cbedaeaba9e827f499
hartmj99 <hartmj99@cs.washington.edu>
2018-12-15 04:22:19 -0800

    fix bugs in cleaner code, writes accurate feather file now

--- chicago_data_cleaner.R
+++ chicago_data_cleaner.R
@@ -3,19 +3,24 @@ source("setup.R")
 # Holds all of Chicago's crime tuples with their ID, Date occured, 
 #   if the crime was violent, and the location in (Lat., Long.)
 chicago.data <- read.csv('data/chicago_crime.csv', 
-                         stringsAsFactors = FALSE) %>%
-                # Add a boolean column "Violent" for if the crime was 
-                #   violent & change Date to be a date object
-                mutate(Violent = Primary.Type %in% c("ROBBERY",
-                                                     "ASSAULT",
-                                                     "SEX OFFENSE",
-                                                     "CRIM SEXUAL ASSAULT",
-                                                     "HOMICIDE"),
-                       Date = as.Date(Date, "%m/%d/%Y"),
-                       Description = Primary.Type,
-                       lat = GetX(Location),
-                       long = GetY(Location)) %>%
-                select(-Location) %>%
-                filter(Date <= "2018-02-08")
+                         stringsAsFactors = FALSE)
 
-write_feather(chicago.data, "data/Chicago_Crime.feather")
+chicago.data <- filter(chicago.data, Location != "")
+
+chicago.data$Latitude = sapply(chicago.data$Location, GetX)
+chicago.data$Longitude = sapply(chicago.data$Location, GetY)
+
+chicago.data <- chicago.data %>%
+    # Add a boolean column "Violent" for if the crime was 
+    #   violent & change Date to be a date object
+    mutate(Violent = Primary.Type %in% c("ROBBERY",
+                                         "ASSAULT",
+                                         "SEX OFFENSE",
+                                         "CRIM SEXUAL ASSAULT",
+                                         "HOMICIDE"),
+           Date = as.Date(Date, "%m/%d/%Y"),
+           Description = Primary.Type) %>%
+    filter(Date <= "2018-02-08")
+
+# Write the data in [ID, Date, Lat, Long, Violent, Description]
+write_feather(chicago.data[, c(1,2,5,6,7,8)], "data/chicago_crime.feather")
