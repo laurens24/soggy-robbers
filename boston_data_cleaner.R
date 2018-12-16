e2a1aa541d99c46a9ef41e8f94aa3fec611350fd
hartmj99 <hartmj99@cs.washington.edu>
2018-12-16 04:15:16 -0800

    Featherize boston data cleaner

--- boston_data_cleaner.R
+++ boston_data_cleaner.R
@@ -1,38 +1,37 @@
-library(dplyr)
-boston.data <- read.csv('data/boston_crime.csv', stringsAsFactors = FALSE)
-boston.data <- filter(boston.data, substring(OCCURRED_ON_DATE, 1, 4) == '2016' | 
-                        substring(OCCURRED_ON_DATE, 1, 4) == '2017' | 
-                        substring(OCCURRED_ON_DATE, 1, 4) == '2018') 
-boston.data <- select(boston.data, INCIDENT_NUMBER, OFFENSE_CODE_GROUP, 
-                      OFFENSE_DESCRIPTION, SHOOTING, OCCURRED_ON_DATE, Location)
+source("setup.R")
 
-boston.data[boston.data$OFFENSE_DESCRIPTION == "THREATS TO DO BODILY HARM", "OFFENSE_CODE_GROUP"] = 
-  "Threats to do bodily harm"
-boston.data <- mutate(boston.data, Violent = SHOOTING == 'Y')
+# 'boston_crime.csv' sourced from https://data.boston.gov/dataset/crime-incident-reports-august-2015-to-date-source-new-system
 
-boston.data$Violent = boston.data$OFFENSE_CODE_GROUP == "Threats to do bodily harm" | 
-  boston.data$OFFENSE_CODE_GROUP == "Aggravated Assault" | 
-  boston.data$OFFENSE_CODE_GROUP == "Arson" |
-  boston.data$OFFENSE_CODE_GROUP == "Residential Burglary" | 
-  boston.data$OFFENSE_CODE_GROUP == "Simple Assault"| 
-  boston.data$OFFENSE_CODE_GROUP == "Ballistics" | 
-  boston.data$OFFENSE_CODE_GROUP == "Offenses Against Child / Family" |
-  boston.data$OFFENSE_CODE_GROUP == "Criminal Harassment" | 
-  boston.data$OFFENSE_CODE_GROUP == "Bomb Hoax" | 
-  boston.data$OFFENSE_CODE_GROUP == "Homicide"| 
-  boston.data$OFFENSE_CODE_GROUP == "Burglary - No Property Taken"
-  
-colnames(boston.data)[colnames(boston.data) == "OFFENSE_CODE_GROUP"] <- "Short.Description"
+violent.categories <- c("Threats to do bodily harm",
+                        "Aggravated Assault", 
+                        "Arson",
+                        "Residential Burglary",
+                        "Simple Assault",
+                        "Ballistics",
+                        "Offenses Against Child / Family",
+                        "Criminal Harassment",
+                        "Bomb Hoax",
+                        "Homicide",
+                        "Burglary - No Property Taken")
 
-boston.data <- mutate(boston.data, Datetype = as.Date(substr(boston.data$OCCURRED_ON_DATE, 1, 10), '%Y-%m-%d'))
+Refine <- function(.data, if.col, if.val, res.col, res.val) {
+  .data[.data[, if.col] == if.val, res.col] = res.val
+  return(.data)
+}
 
-colnames(boston.data)[colnames(boston.data) == "Datetype"] <- "Date"
-
-boston.data <- filter(boston.data, Date <= as.Date("02/08/2018", "%m/%d/%Y"))
-boston.data <- mutate(boston.data,
-                      Lat = getX(Location),
-                      Long = getY(Location))
-
-boston.data <- select(boston.data, Short.Description, Date, Location, Violent)
-
-write_feather(boston.data, "data/Boston_Crime.feather")
+read.csv('data/boston_crime.csv', stringsAsFactors = FALSE) %>% 
+    rename(ID = INCIDENT_NUMBER,
+           Date = OCCURRED_ON_DATE,
+           Latitude = Lat,
+           Longitude = Long,
+           Description = OFFENSE_CODE_GROUP) %>%
+    mutate(Date = as.Date(Date)) %>%
+    filter(Date >= "2016-01-01" & Date <= "2018-02-08",
+           Latitude != -1,
+           !is.na(Latitude)) %>%
+    Refine("OFFENSE_DESCRIPTION", "THREATS TO DO BODILY HARM", 
+           "Description", "Threats to do bodily harm") %>%
+    mutate(Violent = Description %in% violent.categories,
+           Description = toupper(Description)) %>%
+    select(ID, Date, Latitude, Longitude, Violent, Description) %>%
+    write_feather("data/boston_crime.feather")
